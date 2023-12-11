83b45cbc02a2fad858ca2e65450e01b753f4cfd3
chaithanyakumar47 <kumarchaithanya.1@gmail.com>
2023-12-11 23:40:18 +0530

    installed Sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -29,17 +29,17 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
+  Product.findById(prodId)
+  .then((product) => {
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
     });
-  });
+  })
+  .catch(err => console.log(err));
+  
 };
 
 exports.postEditProduct = (req, res, next) => {
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '*hQpgU}f!bY2'
+const sequelize = new Sequelize('node-complete', 'root', '*hQpgU}f!bY2', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
